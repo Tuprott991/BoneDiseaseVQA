c2d18a14b6a87ef6a834bb819872a409d872b4ad
Tuprott991 <nvtu22@clc.fitus.edu.vn>
2025-04-09 13:51:12 +0700

    Update

--- model_arch.py
+++ model_arch.py
@@ -1,14 +1,24 @@
 import torch
 from torchvision import transforms
-from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration
+from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration, BitsAndBytesConfig
 
 def get_model_(model_name="Salesforce/instructblip-flan-t5-xl"):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,  # hoặc load_in_4bit=True
+        llm_int8_threshold=6.0,
+        llm_int8_skip_modules=None,
+    )
+
     # Load processor and model
     processor = InstructBlipProcessor.from_pretrained(model_name)
-    model = InstructBlipForConditionalGeneration.from_pretrained(model_name)
+    model = InstructBlipForConditionalGeneration.from_pretrained(
+        model_name,
+        device_map="auto",
+        quantization_config=quantization_config
+    )
     model = model.to(device)
 
     # Define image transformation (optional if you're using the processor)
